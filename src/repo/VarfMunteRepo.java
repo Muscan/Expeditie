908540bfea69152008db141746d26004cf30092c
Mihai <mcci0023@scs.ubbcluj.ro>
2021-03-12 17:47:18 +0200

    Added Repo for clases AlpinistRepo and ArticolRepo

--- src/repo/VarfMunteRepo.java
+++ src/repo/VarfMunteRepo.java
@@ -1,4 +1,23 @@
 package repo;
 
-public class VarfMunteRepo{
+import model.VarfMunte;
+
+import java.util.List;
+
+public class VarfMunteRepo implements Repository<VarfMunte>{
+    @Override
+    public void save(VarfMunte entity) {
+
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+
+    @Override
+    public List<VarfMunte> findAll() {
+        return null;
+    }
+
 }
