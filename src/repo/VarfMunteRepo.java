ba4a7c61cb94eb7f9398949c315097be5af54b9b
Mihai <mcci0023@scs.ubbcluj.ro>
2021-03-12 17:53:03 +0200

    Implementation for the class VarfMunteRepo

--- src/repo/VarfMunteRepo.java
+++ src/repo/VarfMunteRepo.java
@@ -1,23 +1,21 @@
 package repo;
-
 import model.VarfMunte;
-
 import java.util.List;
 
 public class VarfMunteRepo implements Repository<VarfMunte>{
+    private List<VarfMunte> varfMunti;
     @Override
     public void save(VarfMunte entity) {
-
+        this.varfMunti.add(entity);
     }
-
     @Override
     public int count() {
-        return 0;
+        return this.varfMunti.size();
     }
 
     @Override
     public List<VarfMunte> findAll() {
-        return null;
+        return this.varfMunti;
     }
 
 }
