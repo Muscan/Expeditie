0454ee3b740156479aa8efe280a81fe98b5e9439
Mihai <mcci0023@scs.ubbcluj.ro>
2021-03-12 16:39:45 +0200

    Added Repo and @Override methods for Alpinist

--- src/repo/AlpinistRepo.java
+++ src/repo/AlpinistRepo.java
@@ -1,4 +1,24 @@
 package repo;
 
-public class AlpinistRepo {
+import model.Alpinist;
+
+import java.util.List;
+
+public class AlpinistRepo implements Repository<Alpinist>{
+    private List<Alpinist> alpinist;
+
+    @Override
+    public void save(Alpinist entity) {
+
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+
+    @Override
+    public List<Alpinist> findAll() {
+        return null;
+    }
 }
